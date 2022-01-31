0f30231cc6a90c952b3642d398299b18e4ff4daa
oleholei <kolomoyets473@gmail.com>
2022-01-31 16:33:04 +0200

    added first styles and first basic components

---
+++ App.styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  wrapper: {
+    padding: 24,
+    marginTop: 30
+  },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: '#000000',
+    borderWidth: 1,
+    padding: 10
+  }
+});
--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Button, TextInput } from 'react-native';
+import { styles } from './App.styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.wrapper}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder='Enter the goal'
+        />
+        <Button title="ADD" onPress={console.log} />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
