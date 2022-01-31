f01c399d67215333366ddee9ab6a273f61eb9e8a
oleholei <kolomoyets473@gmail.com>
2022-01-31 16:49:36 +0200

    playing with flexbox

--- App.styles.ts
+++ App.styles.ts
@@ -15,5 +15,21 @@ export const styles = StyleSheet.create({
     borderColor: '#000000',
     borderWidth: 1,
     padding: 10
+  },
+  squareWrapper: {
+    width: '100%',
+    marginTop: 50,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'stretch'
+  },
+  square: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 100,
+    minHeight: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1
   }
 });
--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import { Text, View, Button, TextInput } from 'react-native';
 import { styles } from './App.styles';
 
 export default function App() {
+  const [blocks, setBlocks] = useState<string[]>([]);
+
+  const onClick = () => {
+    setBlocks(blocks => [...blocks, `${blocks.length + 1}`]);
+  }
+
   return (
     <View style={styles.wrapper}>
       <View style={styles.container}>
@@ -9,7 +16,18 @@ export default function App() {
           style={styles.input}
           placeholder='Enter the goal'
         />
-        <Button title="ADD" onPress={console.log} />
+        <Button title="ADD" onPress={onClick} />
+      </View>
+
+      <View style={styles.squareWrapper}>
+        {blocks.map((item, index) => (
+          <View
+            style={{ ...styles.square}} 
+            key={item + index}
+          >
+            <Text>{`block-${item}`}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
